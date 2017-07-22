44c295f716598119d37ecbfd1d62d034d563ea4b
Andrea Barberio <insomniac@slackware.it>
2017-07-22 18:01:20 +0100

    Added --quiet and basic stats

--- nsidenumerator.py
+++ nsidenumerator.py
@@ -20,7 +20,7 @@ def parse_args():
     parser.add_argument(
         'target', help='The target DNS server.')
     parser.add_argument(
-        '-q', '--qname', default='.',
+        '-n', '--qname', default='.',
         help='The DNS name to query for. Default: %(default)r')
     parser.add_argument(
         '-t', '--timeout', type=float, default=1.,
@@ -35,8 +35,14 @@ def parse_args():
         help='Enumerate DNS servers using the specified number of paths. '
         'Default: %(default)s')
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
-        help='Print verbose output')
-    return parser.parse_args()
+            help='Print verbose output. Default: %(default)s')
+    parser.add_argument('-q', '--quiet', action='store_true', default=False,
+            help='Print the minimum necessary information. Default: %(default)s')
+
+    args = parser.parse_args()
+    if args.verbose is True and args.quiet is True:
+        raise parser.error('--quiet and --verbose are mutually exclusive')
+    return args
 
 
 def resolve(name, qtype='A'):
@@ -58,28 +64,43 @@ def main():
 
     start_sport = args.sport
     if args.enumerate is not None:
-        print('Enumerating {} paths'.format(args.enumerate))
+        if not args.quiet:
+            print('Enumerating {} paths'.format(args.enumerate))
         end_sport = start_sport + args.enumerate
     else:
         end_sport = start_sport
 
     servers = set()
+    total_queries = 0
+    timeouts = 0
     for sport in range(start_sport, end_sport + 1):
         if args.verbose:
             print('DNS query to {}({}). Qname: {!r}, sport: {}, dport: {}, '
                   'timeout {}'.format(
                       args.target, target, args.qname, sport, args.dport,
                       args.timeout))
-        ans = dns.query.udp(q, target, timeout=args.timeout,
-                source_port=sport, port=args.dport)
+        total_queries += 1
+        try:
+            ans = dns.query.udp(q, target, timeout=args.timeout,
+                    source_port=sport, port=args.dport)
+        except dns.exception.Timeout:
+            timeouts += 1
+            continue
         for opt in ans.options:
             if opt.otype == dns.edns.NSID:
                 servers.add(opt.data)
 
     hint = '' if len(servers) > 0 else ' (target not supporting NSID?)'
-    print('Found {} servers{}'.format(len(servers), hint))
+    if not args.quiet:
+        print('Found {} servers{}'.format(len(servers), hint))
     for server in sorted(servers):
         print(server)
+
+    if not args.quiet:
+        print()
+        print('Statistics:')
+        print('Total DNS queries: {}'.format(total_queries))
+        print('Timeouts         : {}'.format(timeouts))
 
 
 if __name__ == '__main__':
