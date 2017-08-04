5e5b9b0066bbb306dad65f35ccc6b93f31a328e7
Andrea Barberio <insomniac@slackware.it>
2017-08-04 10:38:30 +0100

    pre-commit filters by extension now

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -3,6 +3,8 @@
 FILES=$(git diff --name-only HEAD)
 for file in $FILES
 do
+    extension="${file##*.}"
+    if [ ! "x$extension" = "xpy" ]; then continue; fi
     mypy --ignore-missing-imports $file || (echo "mypy failed on $file" ; exit 1)
 done
 
